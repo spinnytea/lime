4dc6ff913d6d5a2c446a82d261aedf1a1a2050b6
Garrett Tjarks <spinnytea@gmail.com>
2014-10-26 14:53:27 -0400

    subgraph match
    match one subgraph to another

--- spec/core/database/subgraph.js
+++ spec/core/database/subgraph.js
@@ -9,7 +9,7 @@ var tools = require('../testingTools');
 describe('subgraph', function() {
   it('init', function() {
     // this is to ensure we test everything
-    expect(Object.keys(subgraph)).to.deep.equal(['Subgraph', 'matcher', 'search']);
+    expect(Object.keys(subgraph)).to.deep.equal(['Subgraph', 'matcher', 'search', 'match']);
     expect(Object.keys(subgraph.Subgraph.prototype)).to.deep.equal(['copy', 'addVertex', 'addEdge']);
     expect(Object.keys(subgraph.matcher)).to.deep.equal(['id', 'filler', 'data']);
     expect(Object.keys(subgraph.matcher.data)).to.deep.equal(['exact', 'similar']);
@@ -282,15 +282,15 @@ describe('subgraph', function() {
           var a = sg.addVertex(subgraph.matcher.filler);
           sg.addEdge(m, links.list.thought_description, a);
 
-          var results = subgraph.search(sg);
-          expect(results.length).to.equal(2);
+          var result = subgraph.search(sg);
+          expect(result.length).to.equal(2);
 
-          var one = results[0];
+          var one = result[0];
           expect(one).to.not.equal(sg);
           expect(one.vertices[m].idea.id).to.deep.equal(mark.id);
           expect(one.vertices[a].idea.id).to.deep.equal(apple.id);
 
-          var two = results[1];
+          var two = result[1];
           expect(two).to.not.equal(sg);
           expect(two.vertices[m].idea.id).to.deep.equal(mark.id);
           expect(two.vertices[a].idea.id).to.deep.equal(banana.id);
@@ -328,7 +328,63 @@ describe('subgraph', function() {
     }); // end clauses
   }); // end search
 
-  describe('matchSubgraph', function() {
-    it.skip('matchSubgraph');
+  describe('match', function() {
+    var mark;
+    var outer, m, a, p;
+    beforeEach(function() {
+      mark = tools.ideas.create();
+      var apple = tools.ideas.create();
+      var price = tools.ideas.create({value: 10});
+      mark.link(links.list.thought_description, apple);
+      apple.link(links.list.thought_description, price);
+
+      outer = new subgraph.Subgraph();
+      m = outer.addVertex(subgraph.matcher.id, mark);
+      a = outer.addVertex(subgraph.matcher.filler);
+      p = outer.addVertex(subgraph.matcher.data.similar, {value: 10});
+      outer.addEdge(m, links.list.thought_description, a);
+      outer.addEdge(a, links.list.thought_description, p);
+
+      subgraph.search(outer);
+      expect(outer.concrete).to.equal(true);
+    });
+
+    describe('match filter', function() {
+      it.skip('srcMapped');
+
+      it.skip('!srcMapped');
+
+      it.skip('dstMapped');
+
+      it.skip('!dstMapped');
+
+      it.skip('not match');
+
+      it.skip('match');
+    });
+
+    it.skip('fail');
+
+    it('success single', function() {
+      var sg = new subgraph.Subgraph();
+      var _p = sg.addVertex(subgraph.matcher.data.similar, {value: 10});
+      var _m = sg.addVertex(subgraph.matcher.id, mark);
+      var _a = sg.addVertex(subgraph.matcher.filler);
+      sg.addEdge(_m, links.list.thought_description, _a);
+      sg.addEdge(_a, links.list.thought_description, _p);
+
+      var result = subgraph.match(outer, sg);
+      expect(result.length).to.equal(1);
+      expect(Object.keys(result[0]).length).to.equal(3);
+      expect(result[0][_m]).to.equal(m);
+      expect(result[0][_a]).to.equal(a);
+      expect(result[0][_p]).to.equal(p);
+    });
+
+    it.skip('success multiple');
+
+    it.skip('disjoint graph');
+
+    it.skip('lone vertex');
   }); // end matchSubgraph
 }); // end subgraph
