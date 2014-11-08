12ecdc233a8d5ced8e460de0aaba30ea48a5d056
Garrett Tjarks <spinnytea@gmail.com>
2014-11-08 15:03:08 -0500

    new gulp task! print skipped tests

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,6 @@ var files = ['config.js', 'spec/**/*.js', 'src/core/**/*.js'];
 gulp.task('run-mocha', ['jshint'], function() {
   return gulp.src(['spec/**/*.js'], {read: false})
     .pipe(mocha({reporter: 'nyan'}));
-    // pro tip:
-    //   reporter: 'list'
-    //   gulp run-mocha | grep '-' | grep -v 'ms'
 });
 
 gulp.task('jshint', [], function () {
@@ -20,4 +17,20 @@ gulp.task('jshint', [], function () {
 
 gulp.task('test', ['run-mocha'], function() {
   gulp.watch(files, ['run-mocha']);
+});
+
+gulp.task('skipped', function() {
+  // hacks!
+  // inline gulp
+  // mocha report: list uses '-' to bullet skipped tests
+  // we are going to grep the output to only include those tests
+  var write_back = process.stdout.write;
+  process.stdout.write = function() {
+    if(arguments[0].indexOf('-') === 7)
+      write_back.apply(process.stdout, arguments);
+  };
+
+  // now, run the tests
+  return gulp.src(['spec/**/*.js'], {read: false})
+    .pipe(mocha({reporter: 'list'}));
 });
