c4905d74560a49ae45ea8b09ea4e739a4b886399
Garrett Tjarks <spinnytea@gmail.com>
2014-11-15 12:26:54 -0500

    oooooooooooooo
    define reports on the command line

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,38 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 
+// define which report we will use for the test
+// 'nyan' is the best, so that is the default
+// 'list' is definitely has it's merits
+// 'json' and 'json-stream' are pretty neat
+// XXX what about HTMLCov
+var reporter = 'nyan';
+process.argv.forEach(function(val, idx, array) {
+  if(val === '-r' && array[idx+1])
+    reporter = array[idx+1];
+});
+
+// print out all the tests that have been skipped
+if(reporter === 'skipped') {
+  reporter = 'list';
+
+  // hacks!
+  // inline gulp
+  // mocha report: list uses '-' to bullet skipped tests
+  // we are going to grep the output to only include those tests
+  var write_back = process.stdout.write;
+  process.stdout.write = function() {
+    if(arguments[0].indexOf('-') === 7)
+      write_back.apply(process.stdout, arguments);
+  };
+}
+
+
 var files = ['config.js', 'spec/**/*.js', 'src/core/**/*.js'];
 
 gulp.task('run-mocha', ['jshint'], function() {
   return gulp.src(['spec/**/*.js'], {read: false})
-    .pipe(mocha({reporter: 'nyan'}));
+    .pipe(mocha({reporter: reporter}));
 });
 
 gulp.task('jshint', [], function () {
@@ -17,20 +44,4 @@ gulp.task('jshint', [], function () {
 
 gulp.task('test', ['run-mocha'], function() {
   gulp.watch(files, ['run-mocha']);
-});
-
-gulp.task('skipped', function() {
-  // hacks!
-  // inline gulp
-  // mocha report: list uses '-' to bullet skipped tests
-  // we are going to grep the output to only include those tests
-  var write_back = process.stdout.write;
-  process.stdout.write = function() {
-    if(arguments[0].indexOf('-') === 7)
-      write_back.apply(process.stdout, arguments);
-  };
-
-  // now, run the tests
-  return gulp.src(['spec/**/*.js'], {read: false})
-    .pipe(mocha({reporter: 'list'}));
 });
