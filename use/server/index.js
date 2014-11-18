1b5ad856fdd7435d1849a096772b648dbc803803
Garrett Tjarks <spinnytea@gmail.com>
2014-11-18 18:42:45 -0500

    hosted!

--- use/server/index.js
+++ use/server/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var express = require('express');
+var path = require('path');
 
 var app = express();
-app.use(express.static('../client'));
+app.use('/vender', express.static(path.join(__dirname, '..', '..', 'bower_components')));
+app.use(express.static(path.join(__dirname, '..', 'client')));
 app.listen(8080);
