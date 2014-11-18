4ff62682ad7542619ed4b17dddaa7efcba82d1cd
Garrett Tjarks <spinnytea@gmail.com>
2014-11-18 17:48:05 -0500

    rooms!

--- use/client/js/wumpus/controllers/app.js
+++ use/client/js/wumpus/controllers/app.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = angular.module('lime.client.wumpus.app', []);
 module.exports.controller('lime.client.wumpus.app', [
-  '$scope',
-  function($scope) {
+  '$scope', 'lime.client.wumpus.room',
+  function($scope, Room) {
     $scope.name = 'example';
 
     $scope.gameConfig = {
@@ -14,6 +14,8 @@ module.exports.controller('lime.client.wumpus.app', [
 
     $scope.generateGame = function() {
       console.log('generate!');
+      console.log(new Room(0, 0, { hasExit: true }));
+      console.log(new Room(0, 0).senses());
     };
   }
 ]);
--- use/client/js/wumpus/wumpusModule.js
+++ use/client/js/wumpus/wumpusModule.js
@@ -1,4 +1,5 @@
 'use strict';
 module.exports = angular.module('lime.client.wumpus', [
   require('./controllers/app').name,
+  require('./services/room').name,
 ]);
