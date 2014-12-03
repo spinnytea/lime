e44d62cbf9777a3dddecdf0184d6c89f49f619b7
Garrett Tjarks <spinnytea@gmail.com>
2014-12-02 20:32:24 -0500

    oops. need to reconnect

--- use/client/js/wumpus/socket.js
+++ use/client/js/wumpus/socket.js
@@ -9,32 +9,32 @@ function disconnect() {
   socket = undefined;
 }
 exports.connect = function($scope, protocol, host) {
-    disconnect();
-    socket = io(protocol + '://' + host + ':3000/wumpus'); // TODO config port
+  disconnect();
+  socket = io(protocol + '://' + host + ':3000/wumpus').connect(); // TODO config port
 
-    socket.on('action', function(which) {
-      var keyCode;
-      function doIt() {
-        game.keydown({ keyCode: keyCode, preventDefault: angular.noop });
-      }
+  socket.on('action', function(which) {
+    var keyCode;
+    function doIt() {
+      game.keydown({ keyCode: keyCode, preventDefault: angular.noop });
+    }
 
-      switch(which) {
-        case 'left': keyCode = 37; break;
-        case 'up': keyCode = 38; break;
-        case 'right': keyCode = 39; break;
-        case 'down': keyCode = 40; break;
-        case 'noop': keyCode = 32; break;
-        case 'grab': keyCode = 71; break;
-        case 'exit': keyCode = 69; break;
-        case 'fire': keyCode = 70; break;
-        default:
-          console.log('invalid action: ' + which);
-      }
+    switch(which) {
+      case 'left': keyCode = 37; break;
+      case 'up': keyCode = 38; break;
+      case 'right': keyCode = 39; break;
+      case 'down': keyCode = 40; break;
+      case 'noop': keyCode = 32; break;
+      case 'grab': keyCode = 71; break;
+      case 'exit': keyCode = 69; break;
+      case 'fire': keyCode = 70; break;
+      default:
+        console.log('invalid action: ' + which);
+    }
 
-      if(keyCode) $scope.$apply(doIt);
-    });
+    if(keyCode) $scope.$apply(doIt);
+  });
 
-    return disconnect;
+  return disconnect;
 };
 
 exports.emit = function(event, message) {
