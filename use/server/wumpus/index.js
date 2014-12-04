172cb23b0e1c70626dbdbcac307fd581b6693d2e
Garrett Tjarks <spinnytea@gmail.com>
2014-12-04 16:13:34 -0500

    TODO list for the wumpus server

--- use/server/wumpus/index.js
+++ use/server/wumpus/index.js
@@ -2,8 +2,10 @@
 
 exports.setup = function(io) {
   io.on('connection', function(socket) {
+    // TODO setup context
     console.log('connected!');
     socket.on('disconnect', function() {
+      // TODO tear down context
       console.log('disconnected!');
     });
 
@@ -13,14 +15,19 @@ exports.setup = function(io) {
     // --
     // later, lemon will send these command itself
     socket.on('command', function(str) {
+      // TODO create an actuator impl for each action
+      // TODO one for discrete; one for continuous
       socket.emit('action', str);
     });
 
     socket.on('config', function(config) {
       console.log(config);
+      // TODO put config data in context
     });
-    socket.on('sense', function(state) {
-      console.log(state);
+    socket.on('sense', function() {
+//      console.log(state);
+      // TODO find agent in context: update
+      // TODO find rooms in context: update
     });
   });
 };
